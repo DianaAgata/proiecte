e952453bba1801013455a0ae0c296c8a2fae7bc7
Diana Agata Leonte <dyaneddd@yahoo.com>
2018-03-04 15:18:31 +0200

    adding resumee and a few projects

---
+++ rps-game/game.js
@@ -0,0 +1,27 @@
+(function(){
+var arr = ['rock', 'paper', 'scissors'];
+var userChoice = arr[Math.floor(Math.random() * 3)];
+console.log('User Choice : ' + userChoice);
+var computerChoice = arr[Math.floor(Math.random() * 3)];
+console.log('Computer choice : ' + computerChoice);
+
+function play() {
+    var rock = arr[0];
+    var paper = arr[1];
+    var scissors = arr[2];
+
+    if (userChoice === computerChoice) {
+        return 'The result is a tie!';
+    } else if(userChoice == rock && computerChoice == scissors || userChoice == paper && computerChoice == rock || userChoice == scissors && computerChoice == paper){
+        return 'User wins'
+    }else{
+        return 'Computer wins'
+    }
+    
+}
+
+console.log(play(userChoice, computerChoice));
+}()); 
+    
+    
+    
